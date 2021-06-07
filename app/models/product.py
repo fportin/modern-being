f6a042551dafea7e432259c94a42b7b18ae9db54
Franco Portin <fportin350@gmail.com>
2021-06-07 11:24:24 -0700

    Creating and seeding products table successful.

--- app/models/product.py
+++ app/models/product.py
@@ -4,10 +4,10 @@ class Product(db.Model):
     __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    brand = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    brand = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     photo = db.Column(db.String, nullable=False)
 
 
